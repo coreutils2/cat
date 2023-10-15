fa433afefde49da57e3af8f9fbcb0a3c57845644
mercy <90040892+paracure@users.noreply.github.com>
2023-10-15 15:39:19 +0530

    some more progress yay!

---
+++ src/constants.rs
@@ -0,0 +1,2 @@
+pub const BUFFER_SIZE: usize = 8192;
+pub const VERSION_STRING: &'static str = env!("CARGO_PKG_VERSION");
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod constants;
+
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{Read, Write, BufRead};
 use std::path::PathBuf;
 use clap::{Parser};
-
+use constants::*;
 
 #[derive(Debug, Parser)]
 struct CliArgs {
@@ -13,55 +15,57 @@ struct CliArgs {
     /// print the version and exit
     #[clap(short = 'V', long)]
     version: bool,
+    /// skip empty lines
+    #[clap(short = 'S', long)]
+    skip_empty_lines: bool,
 }
 
-const VERSION_STRING: &'static str = env!("CARGO_PKG_VERSION");
 
-
-fn main() {
+// TODO: ERROR MESSAGES
+fn main() -> anyhow::Result<()> {
+    let mut stdout = std::io::stdout();
+    let mut stderr = std::io::stderr();
     let args = CliArgs::parse();
     if args.version {
-        println!("{}", VERSION_STRING);
-        return;
+        writeln!(stdout, "{}", VERSION_STRING)?;
     }
 
     // read stdin if no files were passed
     if args.files.len() == 0 {
         loop {
             let mut input = String::new();
-            std::io::stdin().read_line(&mut input)
-                .map_err(|e| { eprintln!("{:?}", e) })
-                .expect("How does writing to stdin fail?");
+            std::io::stdin().read_line(&mut input)?;
         }
     }
 
     // read all files
     for file_path in args.files {
-        let file = File::open(&file_path);
-        let reader = match file {
-            Ok(ok) => { BufReader::new(ok) }
-            Err(err) => {
-                eprintln!("Error: {err:?}");
-                return;
+        let mut file = File::open(&file_path)?;
+        let mut buf = vec![0; BUFFER_SIZE];
+        match args.number_lines {
+            true => {
+                // TODO: fix this
+                writeln!(stderr, "Error: This flag has not been implemented yet!")?;
+                return anyhow::Ok(());
             }
-        };
-        for (index, line) in reader.lines().enumerate() {
-            match line {
-                Ok(ref l) => {
-                    match args.number_lines {
+            false => {
+                loop {
+                    let bytes_read = file.read(&mut buf)?;
+                    if bytes_read == 0 { break; }
+                    match args.skip_empty_lines {
                         true => {
-                            println!("{index} {}", line.expect("Well shit."));
+                            // TODO: fix
+                            writeln!(stderr, "Error: This flag has not been implemented yet!")?;
+                            return anyhow::Ok(());
                         }
-                        false => {
-                            println!("{}", l);
-                        }
-                    }
-                }
-                Err(err) => {
-                    eprintln!("Error: {err:?}");
-                    return;
+                        false => {}
+                    };
+                    let content = String::from_utf8_lossy(&buf);
+                    writeln!(stdout, "{content}")?;
+                    stdout.flush()?;
                 }
             }
         }
     }
+    anyhow::Ok(())
 }
